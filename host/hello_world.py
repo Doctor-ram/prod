c51326f849efa73c1b0380aad25a2c36c400980f
doctor-ram <dr.ramachandran01@gmail.com>
2025-06-23 04:22:55 +0530

    DOne

--- host/hello_world.py
+++ host/hello_world.py
@@ -1,6 +1,11 @@
+# hello_world.py
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return "Hello, World from Flask!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
